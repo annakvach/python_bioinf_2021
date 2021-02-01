3a8d7d615bad912bd8c7d5f12be9886eb3cc11d6
annakvach <ay.kvach.r@gmail.com>
2021-02-01 19:05:36 +0300

    line plot

--- pic.py
+++ pic.py
@@ -4,27 +4,35 @@ import pandas as pd
 # line plot for small data
 
 # import data.csv
-headers = ["Zooid bud", "f", "Degenerating zooid", "Brown body", "br", "op", "pbo", "emb", "sum"]
+headers = ["Zooid bud", "Feeding zoods", "Degenerating zooid", "Brown body", "br", "op", "pbo", "emb", "sum"]
 data = pd.read_csv('data.csv', sep=';', names=headers)
 data = data.astype(int)
 
 # sorting
 data.set_index("sum", inplace=True)  # replace index col
 data_sort = data.sort_values(by=["sum"], ascending=[True])
+
 data_degener = data_sort[["Brown body", "Degenerating zooid", "Zooid bud"]]
+data_feed = data_sort["Feeding zoods"]
 
-# rcParams
-
-plt.rcParams['figure.figsize'] = [18, 9]
-plt.rcParams['font.size'] = 15
+# Plots
+plt.rcParams['figure.figsize'] = [18, 7]
+plt.rcParams['font.size'] = 13
 plt.rcParams['savefig.bbox'] = 'tight'
 
-data_degener.plot(color=["darkred", "red", "cyan"])
+figure, axes = plt.subplots(nrows=2, ncols=1)
+# color
+data_degener.plot(color=["darkred", "red", "cyan"], ax=axes[0])
+data_feed.plot(color=["purple"], ax=axes[1])
 
-plt.title('Number of "degenerate/ing" zooids vs colony size')
-plt.xlabel('All zooids in the colony')
-plt.ylabel('Number of the degenerate/ing zooids')
-plt.grid()
+# labels
+plt.suptitle('Number of zooid of different types vs colony size')
+
+axes[0].set_ylabel("'Young' and 'old' zooids")
+axes[1].set_ylabel("Feeding zooids")
+
+axes[0].set_xlabel("")
+axes[1].set_xlabel("Colony size (number of zooids)")
 
 plt.show()
 
