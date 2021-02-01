532d1046d8fb27bbba35f22b24adc8a460c57322
annakvach <ay.kvach.r@gmail.com>
2021-02-01 18:34:28 +0300

    ~ line plot

--- pic.py
+++ pic.py
@@ -4,24 +4,27 @@ import pandas as pd
 # line plot for small data
 
 # import data.csv
-headers = ["bud", "f", "Degenerating", "Brown body", "br", "op", "pbo", "emb", "sum"]
+headers = ["Zooid bud", "f", "Degenerating zooid", "Brown body", "br", "op", "pbo", "emb", "sum"]
 data = pd.read_csv('data.csv', sep=';', names=headers)
 data = data.astype(int)
 
 # sorting
 data.set_index("sum", inplace=True)  # replace index col
 data_sort = data.sort_values(by=["sum"], ascending=[True])
+data_degener = data_sort[["Brown body", "Degenerating zooid", "Zooid bud"]]
 
+# rcParams
 
-data_degener = data_sort[["Brown body", "Degenerating"]]
+plt.rcParams['figure.figsize'] = [18, 9]
+plt.rcParams['font.size'] = 15
+plt.rcParams['savefig.bbox'] = 'tight'
 
-data_degener.plot()
+data_degener.plot(color=["darkred", "red", "cyan"])
 
-plt.title('Number of "degenerate/ing" modules vs colony size')
-plt.xlabel('Colony size')
-plt.ylabel('Degenerate/ing zooids')
+plt.title('Number of "degenerate/ing" zooids vs colony size')
+plt.xlabel('All zooids in the colony')
+plt.ylabel('Number of the degenerate/ing zooids')
 plt.grid()
 
 plt.show()
-
 
