48659acace48d28bfd48d84e04526ff90faaf197
annakvach <ay.kvach.r@gmail.com>
2021-02-06 19:05:00 +0300

    random_task_2

--- chaos.py
+++ chaos.py
@@ -1,61 +1,105 @@
 import numpy as np
-import random
 import time
-from itertools import accumulate
-from operator import mul
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
-# for time
-size = 5
-progression = list(accumulate([10] * size, mul))
-list_random = []
-list_np = []
+# function to test is the list sort or not
 
-print("------------------------ START random ------------------------ ")
-for i in progression:
-    print("SIZE: : ", i)
-    start_random = time.time_ns()
-    for j in range(i):
-        random.uniform(0, 1)
-    end_random = time.time_ns()
-
-    print("-------------- TIME (sec): ", (end_random - start_random) / 10 ** 9)
-    list_random.append((end_random - start_random) / 10 ** 9)
-
-print()
-
-print("------------------------ START np ------------------------ ")
-
-for i in progression:
-    print("SIZE: : ", i)
-    start_np = time.time_ns()
-    np.random.uniform(0, 1, i)
-    end_np = time.time_ns()
-
-    print("-------------- TIME (sec): ", (end_np - start_np) / 10 ** 9)
-    list_np.append((end_np - start_np) / 10 ** 9)
-
-print()
-print("Random: ", list_random)
-print("Numpy:  ", list_np)
-
-# for plot
-y_ord = range(1, size + 1)
+def SORT_OR_NOT(my_random_list):
+    l = my_random_list[0]
+    for r in my_random_list:
+        if l > r:
+            return False # not sorted
+        l = r
+    return True # sorted
 
 
-plt.rcParams['figure.figsize'] = [12, 8]
-plt.rcParams['font.size'] = 20
-plt.rcParams['savefig.bbox'] = 'tight'
+# generate experiment design list
+samples = 10
+repetitions = 5
+arith = []
 
-plt.plot(y_ord, list_random,  color="cyan", label="Random")
-plt.plot(y_ord, list_np, color="darkred", label="Numpy")
+for j in range(1, samples + 1):
+    for _ in range(1, repetitions + 1):
+        arith.append(j)
 
-plt.title('The relationship between the calculation time of random numbers and their number')
-plt.xlabel('Number of random numbers')
-plt.ylabel('Time (sec)')
-plt.legend()
-plt.grid(True)
+print(arith)
 
+# experiment
+cycle_timer = 0
+time_list = []
+
+for i in arith:
+    my_random_list = np.random.randint(1, i + 1, i)
+    print(my_random_list)
+    otvet = SORT_OR_NOT(my_random_list)
+    cycle_timer += 1
+
+    print("=============================================== CYCLE:", cycle_timer, "  REP:", i)
+
+    start_monkey = time.time_ns()
+    while otvet == False:
+        s = np.random.permutation(my_random_list)
+        otvet = SORT_OR_NOT(s)
+
+    end_monkey = time.time_ns()
+
+    time_list.append((end_monkey - start_monkey) / 10 ** 9)
+    print("TIME (sec): ", (end_monkey - start_monkey) / 10 ** 9)
+
+
+print("********************** ")
+print(time_list)
+
+
+# statistics calculate
+temp_list = []
+
+mean_list = []
+std_list = []
+
+temp = 0
+
+
+print("```````````````````````````````````````````````````")
+print("```````````````````````````````````````````````````")
+print("```````````````````````````````````````````````````")
+print("```````````````````````````````````````````````````")
+print("```````````````````````````````````````````````````")
+for j in [i for i in range(0, samples * repetitions, repetitions)]:
+    temp_list = [time_list[j], time_list[j + 1], time_list[j + 2]]
+    print(time_list)
+
+    mean_list.append(np.mean(temp_list))
+    print(mean_list)
+
+    std_list.append(np.std(temp_list))
+    print(std_list)
+
+    temp_list = []
+
+print("```````````````````````````````````````````````````")
+print("```````````````````````````````````````````````````")
+print("```````````````````````````````````````````````````")
+
+# check data for plot
+print(len(time_list), time_list)
+print(len(mean_list), mean_list)
+print(len(std_list), std_list)
+
+numbers = [i for i in range(1, samples + 1)]
+print(numbers)
+
+# PLOT
+
+x_pos = np.arange(len(numbers))
+
+
+fig, ax = plt.subplots()
+ax.bar(x_pos, mean_list, yerr=std_list, color='darkred', align='center', alpha=0.7, ecolor='black', capsize=10)
+ax.set_ylabel('Time (sec)')
+ax.set_xticks(x_pos)
+ax.set_xticklabels(numbers)
+ax.set_title('Time vs number')
+ax.yaxis.grid(True)
 plt.show()
