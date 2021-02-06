40e05d50f06c23295352ad510247d1f21da9fdc4
annakvach <ay.kvach.r@gmail.com>
2021-02-06 19:27:01 +0300

    random_task_2 + pic_2

--- random_2.py
+++ random_2.py
@@ -9,21 +9,19 @@ def SORT_OR_NOT(my_random_list):
     l = my_random_list[0]
     for r in my_random_list:
         if l > r:
-            return False # not sorted
+            return False  # not sorted
         l = r
-    return True # sorted
-
+    return True  # sorted
 
 # generate experiment design list
 samples = 10
-repetitions = 5
-arith = []
+repetitions = 15
+arith = []  # experiment design list
 
 for j in range(1, samples + 1):
     for _ in range(1, repetitions + 1):
         arith.append(j)
 
-print(arith)
 
 # experiment
 cycle_timer = 0
@@ -47,59 +45,37 @@ for i in arith:
     time_list.append((end_monkey - start_monkey) / 10 ** 9)
     print("TIME (sec): ", (end_monkey - start_monkey) / 10 ** 9)
 
-
-print("********************** ")
-print(time_list)
-
-
 # statistics calculate
 temp_list = []
-
 mean_list = []
 std_list = []
-
 temp = 0
 
-
-print("```````````````````````````````````````````````````")
-print("```````````````````````````````````````````````````")
-print("```````````````````````````````````````````````````")
-print("```````````````````````````````````````````````````")
-print("```````````````````````````````````````````````````")
 for j in [i for i in range(0, samples * repetitions, repetitions)]:
     temp_list = [time_list[j], time_list[j + 1], time_list[j + 2]]
-    print(time_list)
-
     mean_list.append(np.mean(temp_list))
-    print(mean_list)
-
     std_list.append(np.std(temp_list))
-    print(std_list)
-
     temp_list = []
 
-print("```````````````````````````````````````````````````")
-print("```````````````````````````````````````````````````")
-print("```````````````````````````````````````````````````")
-
-# check data for plot
-print(len(time_list), time_list)
-print(len(mean_list), mean_list)
-print(len(std_list), std_list)
-
 numbers = [i for i in range(1, samples + 1)]
-print(numbers)
 
 # PLOT
 
-x_pos = np.arange(len(numbers))
+plt.rcParams['figure.figsize'] = [12, 8]
+plt.rcParams['font.size'] = 20
+plt.rcParams['savefig.bbox'] = 'tight'
 
+x_pos = np.arange(len(numbers))
 
 fig, ax = plt.subplots()
 ax.bar(x_pos, mean_list, yerr=std_list, color='darkred', align='center', alpha=0.7, ecolor='black', capsize=10)
 ax.set_ylabel('Time (sec)')
+ax.set_xlabel('Number of items in the random list (n)')
+
 ax.set_xticks(x_pos)
 ax.set_xticklabels(numbers)
-ax.set_title('Time vs number')
+ax.set_title('Sorting time of the random list vs their number of items')
 ax.yaxis.grid(True)
 plt.show()
+
+plt.savefig('Random_task_2_pic.png')
