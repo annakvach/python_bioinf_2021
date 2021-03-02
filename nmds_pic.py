c08a646b9a0383b9703b00699bcc920f936c0652
annakvach <ay.kvach.r@gmail.com>
2021-03-02 19:50:35 +0300

    my favorite plots (2)

--- nmds_pic.py
+++ nmds_pic.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pandas.plotting import parallel_coordinates
 import plotly.express as px
+import squarify
 
 path_to_data = "C:/my_mnt_c/Code/my_github/python_bioinf_2021/rd.csv"
 
@@ -13,6 +14,8 @@ headers = ["Month", "Zooid bud",
       "Polypide, ovary", "Bb, polypide, ovary",
       "Embryos", "All zoids", "Diameter_age"]
 data = pd.read_csv(path_to_data, sep=',', names=headers)
+
+
 # data cols type
 data[["Month", "Zooid bud",
       "Feeding zoods", "Degenerating zooid",
@@ -26,18 +29,101 @@ data[["Month", "Zooid bud",
 
 data[["Diameter_age"]] = data[["Diameter_age"]].astype(str)
 
-# ["< 5 month", "5 month - 1 year", "> 1 year", "> 2 years"
+# plot to browser (not very useful plot)
+cols = ["Zooid bud",
+      "Feeding zoods","Degenerating zooid",
+      "Brown body", "Bb, polypide",
+      "Polypide, ovary", "Bb, polypide, ovary",
+      "Embryos", "All zoids"]
 
 
-# plot
+fig = px.parallel_coordinates(data,
+                              color="All zoids",
+                              dimensions=cols,
+                              color_continuous_scale=px.colors.diverging.Tealrose,
+                              width=1000,
+                              title="Colony size and zooids of different types")
 
-# print(data)
+fig.show()
 
 
+# plot to png
+
+## Parallel plot
+plt.figure(figsize=(14, 6), dpi=100)
 parallel_coordinates(data, 'Diameter_age',
                      colormap=plt.get_cmap("Set3"),
                      linewidth=1.5,
                      alpha=0.8)
 plt.ylabel('Colony size and age $\\rightarrow$', fontsize=12)
 
-plt.show()
+
+plt.savefig("1.png")
+plt.close()
+
+
+# Treemap of colonies of different age
+df = data.groupby('Diameter_age').size().reset_index(name='sum') # create dataframe with groups (age) and number of colonies (sum)
+
+labels = df.apply(lambda x: str(x[0]) + "\n [" + str(x[1]) + "]", axis=1) # apply to each row special form
+
+sizes = df['sum'].values # get values from col sum
+colors = ['lightseagreen', 'skyblue', 'thistle', 'maroon']
+
+
+plt.figure(figsize=(14, 6), dpi=100)
+plt.axis('off') # delete axis
+squarify.plot(sizes=sizes, label=labels, color=colors, alpha=.8)
+plt.title('Treemap of colonies of different age')
+
+
+
+plt.savefig("2.png")
+plt.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ----------------------------------------------------------------------------------------------
+"""
+a = data[["Month", "Zooid bud", "Feeding zoods", "Degenerating zooid",
+          "Brown body", "Bb, polypide",
+          "Polypide, ovary", "Bb, polypide, ovary",
+          "Embryos", "All zoids"]].astype(int)
+
+b = data[["Diameter_age"]].astype(str).iloc[0]
+
+features = ["Month", "Zooid bud", "Feeding zoods", "Degenerating zooid",
+          "Brown body", "Bb, polypide",
+          "Polypide, ovary", "Bb, polypide, ovary",
+          "Embryos", "All zoids"]
+
+# "< 5 month", "5 month - 1 year", "> 1 year", "2 years"
+classes = ["Diameter_age"]
+
+visualizer = parallel_coordinates(a, b, classes=classes, features=features)
+
+"""
+
+# ----------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
