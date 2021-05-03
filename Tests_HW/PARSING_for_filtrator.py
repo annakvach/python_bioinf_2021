e155586ef71ea7da5ed11011fc4e4a27a98416f1
annakvach <ay.kvach.r@gmail.com>
2021-05-03 03:37:19 +0300

    pytest.raises(ValueError) в test_flag_check

--- Tests_HW/PARSING_for_filtrator.py
+++ Tests_HW/PARSING_for_filtrator.py
@@ -11,21 +11,36 @@ def check_type_for_number(potential_number):
     try:
         float(potential_number)
         return True
-    except ValueError:
+    except TypeError:
+        print('TypeError')
         return False
+    except ValueError:
+        print('ValueError')
+        return False
+    except NameError:
+        print('NameError')
+        return False
+
 
 
 def check_int(number):
     try:
         int(number)
         return True
+    except TypeError:
+        print('TypeError')
+        return False
     except ValueError:
+        print('ValueError')
+        return False
+    except NameError:
+        print('NameError')
         return False
 
 
 # check is the argument in sys.argv[1:] is a flag
 def is_flag(x):
-    if "--" in x:
+    if "--" in x and iter(x):
         return True
     else:
         return False
@@ -42,6 +57,7 @@ def flag_check(list_flags_and_options):
                                  f'\t --min_length \n'
                                  f'\t --gc_bounds \n'
                                  f'\t --keep_filtered \n')
+
         else:
             continue
 
@@ -90,15 +106,15 @@ def fastq_file_check(file_extinction):
 '''str_path_to_dir_with_files, str_input_fastq_file_name, str_path_to_input_fastq_file = fastq_file_check(".fastq")'''
 
 # 2 part
-if (is_flag(list_flags_and_options[0]) == False) and (".fastq" not in str(list_flags_and_options[0])):
+'''if (is_flag(list_flags_and_options[0]) == False) and (".fastq" not in str(list_flags_and_options[0])):
     sys.exit(
         "You should specify at least 1 flag and file name to start filtering!(or just a file name to "
-        "remove error message (although filtration won't start)")
+        "remove error message (although filtration won't start)")'''
 
 
 # 3 part
 
-def OUT_PUT_BASE_NAME(flag, list_flags_and_options):
+def OUT_PUT_BASE_NAME(flag):
     if argument_after_flag(flag):
         value = list_flags_and_options[list_flags_and_options.index(flag) + 1]
         return str(value)
@@ -111,29 +127,36 @@ def OUT_PUT_BASE_NAME(flag, list_flags_and_options):
 
 # 4 part
 
-def MIN_LENGTH(flag, list_flags_and_options):
+def MIN_LENGTH(flag):
+    # flag = "--min_length"
+
+
     if argument_after_flag(flag):
         value = list_flags_and_options[list_flags_and_options.index(flag) + 1]
         if check_type_for_number(value):
             if check_int(value):
-                if not int(value) > 0:
+
+                if int(value) < 0:
                     raise ValueError(f'Min length value can`t be negative: [{value}]')
-                return int(value)
+                else:
+                    return int(value)
             else:
                 raise ValueError(f'Min length value must be integer number only (int): [{value}]')
+
         else:
             raise TypeError(
                 f'Min length value must be non-negative real integer number only (int): [{value}]')
     else:
+        print('EEEEEEEEEEEEEEEENNNNNNNNNNNNNNDDDDDDDD', flag)
         return 0
 
 
-'''int_min_length = MIN_LENGTH("--min_length")'''
+int_min_length = MIN_LENGTH("--min_length")
 
 
 # 5 part
 
-def LEFT_GC_BOUNDS(flag, list_flags_and_options):
+def LEFT_GC_BOUNDS(flag):
     if argument_after_flag(flag):
         value = list_flags_and_options[list_flags_and_options.index(flag) + 1]
         if check_type_for_number(value):
@@ -147,7 +170,7 @@ def LEFT_GC_BOUNDS(flag, list_flags_and_options):
         return 0.0
 
 
-def RIGHT_GC_BOUNDS(flag, list_flags_and_options):
+def RIGHT_GC_BOUNDS(flag):
 
     if argument_after_after_flag(flag):
         value = list_flags_and_options[list_flags_and_options.index(flag) + 2]
@@ -174,28 +197,13 @@ float_left_gc_bound = LEFT_GC_BOUNDS("--gc_bounds")
 float_right_gc_bound = RIGHT_GC_BOUNDS("--gc_bounds")'''
 
 
-def error_output_permission(flag, list_flags_and_options):
+def error_output_permission(flag):
     if flag in list_flags_and_options:
         return True
     else:
         return False
 
-
-
-# проверяю аргуенты и флаги и сохраняю их для дальнейшего пользования
-flag_check(list_flags_and_options)
-str_path_to_dir_with_files, str_input_fastq_file_name, str_path_to_input_fastq_file = fastq_file_check(".fastq")
-str_new_name = OUT_PUT_BASE_NAME("--output_base_name", list_flags_and_options)
-int_min_length = MIN_LENGTH("--min_length", list_flags_and_options)
-gc_bound_adequacy(LEFT_GC_BOUNDS("--gc_bounds"), RIGHT_GC_BOUNDS("--gc_bounds"), list_flags_and_options)
-float_left_gc_bound = LEFT_GC_BOUNDS("--gc_bounds", list_flags_and_options)
-float_right_gc_bound = RIGHT_GC_BOUNDS("--gc_bounds", list_flags_and_options)
-error_output_permission = error_output_permission("--keep_filtered", list_flags_and_options)
-
-
-# IGNAT PART
-
-# GC count caculation
+# ==================================================
 def gc_count(read):
     count = 0
     for base in read:
@@ -204,66 +212,32 @@ def gc_count(read):
     return count * 100 / len(read)
 
 
-# ez
-# check for good reads
+'''def passed(read, int_min_length, float_left_gc_bound, float_right_gc_bound):
+
+    if len(read) > int_min_length:  # if l(read) < min length => F
+        l = True
+    else:
+        l = False
+
+    if gc_count(read) > float_left_gc_bound  and  gc_count(read) < float_right_gc_bound:
+        b = True
+    else:
+        b = False
+
+    return b and l'''
+
+
+
 def passed(read, int_min_length, float_left_gc_bound, float_right_gc_bound, list_flags_and_options):
     if "--min_length" in list_flags_and_options:
         if len(read) > int_min_length:  # if l(read) < min length => F
             l = True
         else:
             l = False
-
     if "--gc_bounds" in list_flags_and_options:
         if gc_count(read) > float_left_gc_bound  and  gc_count(read) < float_right_gc_bound:
             b = True
         else:
             b = False
+    
     return b and l
-
-
-
-
-
-# ez
-# save in file
-def file_output(readlines, file):
-    file.write('\n'.join(readlines) + '\n')
-
-
-# ez af
-# open file
-fastq_input = open(str_input_fastq_file_name, 'r')
-# read all input lines
-all_reads = fastq_input.read().splitlines()
-# N of reads in file
-total_reads = str(len(all_reads) // 4)
-# create output blank file and creating failed .fastq if necessary
-fastq_passed = open(str_new_name + '_passed.fastq', 'w')
-if error_output_permission == True:
-    fastq_failed = open(str_new_name + '_failed.fastq', 'w')
-
-# counter for passed/failed reads and their filtration
-reads_passed = 0
-reads_failed = 0
-for i in range(0, len(all_reads), 4):  # we need to read lines by 4, e.g. 1-4, 5-8, 9-12 etc.
-    current_read = all_reads[i:i + 4]  # reading 2nd line where ATGC's are
-    if i == 0:  # check if empty
-        print()
-    if passed(current_read[1], int_min_length, float_left_gc_bound, float_right_gc_bound, list_flags_and_options):
-        file_output(current_read, fastq_passed)
-        reads_passed += 1
-    else:
-        if error_output_permission == True:
-            file_output(current_read, fastq_failed)
-        reads_failed += 1
-
-# don't forget to close file
-print('Done!')
-print('Total reads number is ' + str_input_fastq_file_name + ': ' + total_reads)
-print(str(reads_passed) + ' (' + str(round(reads_passed * 100 / int(total_reads), 2)) + '%) reads passed filtering.')
-print(str(reads_failed) + ' (' + str(round(reads_failed * 100 / int(total_reads), 2)) + '%) reads failed filtering.')
-
-fastq_passed.close()
-fastq_input.close()
-if error_output_permission == True:
-    fastq_failed.close()
--- Tests_HW/tests/test_check_type_for_number.py
+++ Tests_HW/tests/test_check_type_for_number.py
@@ -4,9 +4,11 @@ from Tests_HW.PARSING_for_filtrator import \
     check_int, \
     is_flag, \
     gc_count,\
-    passed
+    passed, \
+    flag_check
 
 
+# Parsing part
 @pytest.mark.parametrize("a, result", [(8, True),
                                        ('vosem', False),
                                        ([8], False),
@@ -16,13 +18,27 @@ def test_NUMBER_for_Check_int_and_Check_type_for_number(a, result):
     assert check_int(a) == result
 
 
+def test_flag_check():
+    """add() должно возникнуть исключение с неправильным типом param."""
+    with pytest.raises(ValueError):
+        flag_check(["--output_bosee_name", "0-o" "--min-length", "10", "--gc_bunds", "50" "--keep_filtered"])
+
+
+
+
+
+
+
+
+
+
+
+# Filtration part
 @pytest.mark.parametrize("read, result_gc_count", [('AAAAAGGGGG', 50.0),
                                                    ('AAAAAA', 0.0),
                                                    ('CCCGGG', 100.0)])
 def test_gc_count(read, result_gc_count):
     assert gc_count(read) == result_gc_count
-
-
 
 
 @pytest.mark.parametrize("read, int_min_length, float_left_gc_bound, float_right_gc_bound, list_flags_and_options, read_destiny",
